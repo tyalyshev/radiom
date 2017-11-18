fbfd1d054b4fb8477b4fded968bfaf278c60d085
timson <t.yalyshev@yandex.ru>
2017-11-18 23:38:24 +0400

    delete double song

--- src/main/java/DbConnect.java
+++ src/main/java/DbConnect.java
@@ -2,6 +2,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
 public class DbConnect {
 
@@ -30,8 +31,38 @@ public class DbConnect {
                 e.printStackTrace();
             }
     }
+    public ArrayList sqlSelectQuerty() {
+        ArrayList<String> timeInBase = new ArrayList<>();
+        try {
+            Class.forName("org.postgresql.Driver");
+            String url = "jdbc:postgresql://192.168.31.149:5432/radiom";
+            String login = "postgres";
+            String password = "postgres";
+            Connection c = DriverManager.getConnection(url, login, password);
+            try {
+                Statement stmt = c.createStatement();
+                ResultSet rs = stmt.executeQuery( "SELECT * FROM maximum ORDER BY id DESC LIMIT 10;" );
+                while (rs.next()){
+                    timeInBase.add(rs.getString("dtime"));
+                }
+                rs.close();
+                stmt.close();
 
-}
+            } finally {
+                c.close();
+
+            }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
+         return timeInBase;
+
+        }
+
+    }
+
+
 
 
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,11 +20,12 @@ public class  Main {
         JsonParser parser = new JsonParser();
         JsonObject mainObject = parser.parse(resultSting).getAsJsonObject();
         JsonArray questions = mainObject.getAsJsonArray("History");
-        JsonObject currObj = mainObject.getAsJsonObject("Current");
+        //JsonObject currObj = mainObject.getAsJsonObject("Current");
 
         ArrayList<String> artistList = new ArrayList<>();
         ArrayList<String> songList = new ArrayList<>();
         ArrayList<String> starttimeList = new ArrayList<>();
+
 
         System.out.println("CURRENT \n");
         //System.out.println(currObj.get("Artist").getAsString());
@@ -34,13 +35,15 @@ public class  Main {
 
         DbConnect pg = new DbConnect();
 
+        ArrayList<String> starttimeListDb = pg.sqlSelectQuerty();
+
         for (JsonElement user : questions) {
             JsonObject userObject = user.getAsJsonObject();
 
             String asr = userObject.get("Artist").getAsString();
-            String asrreg = asr.replace("\'","\''");
+            String asrreg = asr.replace("\'", "\''");
             String ssr = userObject.get("Song").getAsString();
-            String ssrreg = ssr.replace("\'","\''");
+            String ssrreg = ssr.replace("\'", "\''");
             String strreg = userObject.get("StartTime").getAsString();
 
             //System.out.println(userObject.get("Artist").getAsString());
@@ -60,17 +63,55 @@ public class  Main {
 
         }
 
+
+
         ArrayList<String> reversArtistList = revers(artistList);
         ArrayList<String> reversSongList = revers(songList);
         ArrayList<String> reverStartTimetList = revers(starttimeList);
+        ArrayList<String> reverStartTimetListDb = starttimeListDb;
+        //ArrayList<String> reverStartTimetListDb = revers(starttimeListDb);
+        ArrayList<String> reverStartTimetListNz = new ArrayList<>();
 
+        for (int i = 0 ; i < reverStartTimetList.size(); i++){
+            reverStartTimetListNz.add(timeNormalize(reverStartTimetList.get(i)));
+        }
+        reverStartTimetList = reverStartTimetListNz;
+
+
+/*
         for (int i = 0; i < reversArtistList.size(); i++){
 
             String sqlExecute = "INSERT INTO maximum VALUES (nextval('auto_rows_id'), '" + reversArtistList.get(i) +
                     "','" + reversSongList.get(i) + "','" + reverStartTimetList.get(i) + "')";
              pg.sqlInsertQuerty(sqlExecute);
         }
-     }
+*/
+
+        for (int i = 0; i < reverStartTimetList.size(); i++) {
+            boolean istina = true;
+            String s = reverStartTimetList.get(i);
+            for (int j = 0; j < reverStartTimetList.size(); j++) {
+
+                if (s.equals(reverStartTimetListDb.get(j))) {
+                    System.out.println(s + " == " + reverStartTimetListDb.get(j));
+                    istina = false;
+                }
+                else {
+                    System.out.println( s +  "!==" + reverStartTimetListDb.get(j));
+                }
+
+
+
+            }
+            if (istina) {
+                pg.sqlInsertQuerty("INSERT INTO maximum VALUES (nextval('auto_rows_id'), '" + reversArtistList.get(i) +
+                        "','" + reversSongList.get(i) + "','" + reverStartTimetList.get(i) + "')");
+            }
+        }
+    }
+
+
+// private function
 
     private static URL createUrl(String link) {
         try {
@@ -101,13 +142,21 @@ public class  Main {
     }
 
     private static ArrayList<String> revers(ArrayList<String> list) {
-        int a = list.size() - 1;
+
         ArrayList<String> result = new ArrayList<>();
-        for (int i  = 0; i < a; i++){
-             result.add(list.get(a - i));
+        int b = list.size() - 1;
+        for (int i = 0; i < list.size(); i++) {
+
+            result.add(list.get(b - i));
         }
         return result;
 
     }
 
-}
+    private static String timeNormalize(String r) {
+        String rr = r.replace("T", " ");
+        String rrr = rr.substring(0, 19);
+        return rrr;
+    }
+
+ }
